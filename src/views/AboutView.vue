00047a1a2f8266ed04d637586edcf71b97b40a39
FailedEx <thereisanimposteramongus@gmail.com>
2023-03-31 21:53:37 +1300

    info change

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -7,9 +7,9 @@
       <li>only the most recent 20 messages are stored</li>
       <li>as soon as I restart the server, all messages history is cleared</li>
       <li>because this is hosted on the school wifi, it is not accessible outside of school</li>
-      <li>because I am hosting this on my computer, the link is my ip address and is not permanent as it changes when I leave school</li>
+      <li>this is hosted on a shitty laptop somewhere in school. the link is permanent.</li>
     </ul>
-    <h5>in other words, what happens in datcord stays in datcord.</h5>
+    <h5>Rule 1: what happens in datcord stays in datcord.</h5>
   </div>
 </template>
 
